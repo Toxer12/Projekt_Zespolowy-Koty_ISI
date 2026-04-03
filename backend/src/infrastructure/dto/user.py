5aabdb0d37cedb1fcac79d7030e7edb2a915aea6
GOstrowski04 <gabrielostrowski2004@gmail.com>
2026-04-03 17:24:30 +0200

    Cleaning up main branch

--- backend/src/infrastructure/dto/user.py
+++
@@ -1,9 +0,0 @@
-from pydantic import UUID4, BaseModel, ConfigDict
-
-class UserDTO(BaseModel):
-    id: UUID4
-    email: str
-    model_config = ConfigDict(
-        from_attributes=True,
-        extra="ignore",
-    )
