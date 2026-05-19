5bb873b6866d935581e6effced38b582be6d7504
Toxer12 <115220195+Toxer12@users.noreply.github.com>
2026-05-19 17:40:07 +0200

    Models registration

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name', 'is_staff', 'is_active']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            )
+        }),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    readonly_fields = ['last_login']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_staff',
+                'is_active',
+            ),
+        }),
+    )
+
+    search_fields = ['email', 'name']
